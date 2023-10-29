999a171dd8b51a4b0863721b614302e8cd8c9475
Pratik-Kumar2402 <pratik.kumar2402@gmail.com>
2023-10-29 16:05:01 +0530

    first-commit

---
+++ footer.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/header_footer.css">
+</head>
+
+<body>
+    <footer class="footer">
+        <p>@ All copyright are reversed.</p>
+        <a href="mailto:authour@example.com">author@example.com</a>
+    </footer>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/header_footer.css">
+</head>
+
+<body>
+    <nav class="navbar">
+        <div class="navbar-icon">
+            <a class="navbar-brand" href="#">Car Rental</a>
+        </div>
+        <div class="navbar-nav">
+            <a class="nav-link" aria-current="page" href="#">Dashboard</a>
+            <a class="nav-link" href="#">Contact</a>
+            <a class="nav-link" href="#">About</a>
+        </div>
+    </nav>
+</body>
+
+</html>
